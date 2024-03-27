eb4db88b0cdf727b7300af6c299d0cb004c63b2b
dongil <az330015@gmail.com>
2024-03-27 16:15:00 +0900

    프로젝트 생성

---
+++ src/main/java/com/groups/schicken/SChickenApplication.java
@@ -0,0 +1,13 @@
+package com.groups.schicken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SChickenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SChickenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/groups/schicken/SChickenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groups.schicken;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SChickenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
