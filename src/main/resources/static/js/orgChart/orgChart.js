ba935d907c9834de85e0a3c922a2942b37070df4
dongil <az330015@gmail.com>
2024-04-01 23:40:04 +0900

    feat. 조직도 만드는 중

--- src/main/resources/static/js/orgChart/orgChart.js
+++ src/main/resources/static/js/orgChart/orgChart.js
@@ -1,14 +1,15 @@
 $(function (){
 
     let datas = [
-        {'id' : '1' , 'parent' : '#' , 'type' : 'wdmsfile', 'text' : 'root', state : {opened : true}},
-        {'id' : '2' , 'parent' : '1' , 'type' : 'wdmsfile', 'text' : '22', state : {opened : true}},
-        {'id' : '3' , 'parent' : '1' , 'type' : 'wdmsdir', 'text' : '33', state : {opened : true}},
-        {'id' : '4' , 'parent' : '2' , 'type' : 'wdmsdir', 'text' : '44', state : {opened : true}},
-        {'id' : '5' , 'parent' : '2' , 'type' : 'arbfile', 'text' : '55', state : {opened : true}},
-        {'id' : '6' , 'parent' : '2' , 'type' : 'arbfile', 'text' : '66', state : {opened : true}},
-        {'id' : '7' , 'parent' : '3' , 'type' : 'arbdir', 'text' : '77', state : {opened : true}},
-        {'id' : '8' , 'parent' : '3' , 'type' : 'arbdir', 'text' : '88', state : {opened : true}}
+        {'id' : '1' , 'parent' : '#' , 'type' : 'dept', 'text' : '사장실', state : {opened : true}},
+        {'id' : '2' , 'parent' : '1' , 'type' : 'person', 'text' : '사장 김범서', state : {opened : true}},
+        {'id' : '3' , 'parent' : '1' , 'type' : 'dept', 'text' : '식품개발부', state : {opened : true}},
+        {'id' : '4' , 'parent' : '1' , 'type' : 'dept', 'text' : '경영지원실', state : {opened : true}},
+        {'id' : '5' , 'parent' : '3' , 'type' : 'person', 'text' : '팀장 조민우', state : {opened : true}},
+        {'id' : '6' , 'parent' : '3' , 'type' : 'person', 'text' : '팀원 남명균', state : {opened : true}},
+        {'id' : '7' , 'parent' : '3' , 'type' : 'person', 'text' : '팀원 이동일', state : {opened : true}},
+        {'id' : '8' , 'parent' : '4' , 'type' : 'person', 'text' : '실장 허석훈', state : {opened : true}},
+        {'id' : '9' , 'parent' : '4' , 'type' : 'person', 'text' : '팀원 김경모', state : {opened : true}}
     ]
 
     $("#org-chart").on('changed.jstree', function (e,data){
@@ -32,14 +33,15 @@ $(function (){
         'core' : {
             'data' : datas
         },
-        plugins : ['types'],
+        plugins : ['types', 'wholerow'],
         'types':{
-            'wdmsfile' : {'li_attr' :{style : "background : #FFDDFF"}},
-            'wdmsdir' : {'li_attr' :{style : "background : #EFDFAA"}},
-            'arbfile' : {'li_attr' :{style : "background : #878681"}},
-            'arbdir' : {'li_attr' :{style : "background : #DDFFDD"}},
-            'default' : {
-                'icon' : 'bi bi-person-circle'
+            'dept' : {
+                'icon': 'bi bi-building'
+            },
+            'person' : {
+                'icon' : 'bi bi-person',
+                'li_attr' : {style : 'background:#7746f8;'},
+                'a_attr' : {style : 'background:#7746f8;'},
             }
         }
     });
