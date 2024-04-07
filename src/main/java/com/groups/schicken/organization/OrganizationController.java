197c5bd8030aadff5ef015f61ede8d091982e33f
dongil <az330015@gmail.com>
2024-04-07 16:53:34 +0900

    feat: 부서 추가하는 기능 일부 만듬
    부서 내선번호 중복 안되게 하고
    depth가 3 넘지 못하게 하고
    sort값을 추가해서 정렬이 되도록 만들어야함

--- src/main/java/com/groups/schicken/organization/OrganizationController.java
+++ src/main/java/com/groups/schicken/organization/OrganizationController.java
@@ -1,5 +1,7 @@
 package com.groups.schicken.organization;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,20 +14,15 @@ import java.util.List;
 @Controller
 @RequestMapping("/organization/*")
 public class OrganizationController {
+    @Autowired
+    private OrganizationService organizationService;
 
     @GetMapping("orgChart")
     @ResponseBody
-    public List<OrgElementVO> getOrgChart(){
-        List<OrgElementVO> list = new ArrayList<>();
+    public ResponseEntity<List<OrgElementVO>> getOrgChart(String opt){
+        List<OrgElementVO> list  = organizationService.getOrgChart(opt);
 
-        String[] parent = "#,0,1,1,1,0,5,5,0,8,8,0,11,11,0,14,14,0,17,17".split(",");
-        String[] text = "S치킨,경영지원부,총무과,재무과,인사과,식품개발부,개발1팀,개발2팀,영업부,영업1팀,영업2팀,마케팅부,마케팅1팀,마케팅2팀,공급망관리부,구매팀,물류팀,가맹관리부,가맹1팀,가맹2팀".split(",");
-
-        for (int i = 0; i < text.length; i++) {
-            list.add(new OrgElementVO(i + "",parent[i], "dept",text[i]));
-        }
-
-        return list;
+        return ResponseEntity.ok(list);
     }
 
     @PostMapping("orgChart")
--- src/main/resources/static/js/orgChart/orgChartPage.js
+++ src/main/resources/static/js/orgChart/orgChartPage.js
@@ -1,17 +1,103 @@
 import oc from "/js/orgChart/orgChart.js";
 
-let selected = "";
-oc.init("org-chart", data=>{
-    if(data.isSelect){
-        selected = data;
-    } else {
-        selected = null;
+const deptAddBtn = document.getElementById("dept-add-btn");
+const deptModBtn = document.getElementById("dept-mod-btn");
+const deptModal = document.getElementById("dept-modal");
+const bsDeptModal = new bootstrap.Modal(deptModal);
+const deptSubmitBtn = document.getElementById("dept-submit-btn");
+const upperName = document.getElementById("upper-name");
+const deptName = document.getElementById("dept-name");
+const deptNumber = document.getElementById("dept-number");
+let selected;
+
+function createOrgChart(){
+    oc.init("org-chart", data=>{
+        if(data.isSelect){
+            selected = data;
+        } else {
+            selected = null;
+        }
+        if(selected != null && data.type === 'dept') {
+            document.querySelectorAll(".dept-btn")
+                .forEach(e=>e.classList.remove("disabled"));
+        } else{
+            document.querySelectorAll(".dept-btn")
+                .forEach(e=>e.classList.add("disabled"));
+        }
+    });
+}
+
+function setModalAddDept(data){
+    upperName.innerText = data.name;
+    upperName.value = data.id;
+}
+
+/**
+ * 부서명, 내선번호, 상위부서가 null 인지 체크
+ * 생성하려면 부서의 depth 가 3 이하인지 확인
+ * 사람의 아래에 생성하는 것은 아닌지 확인
+ */
+function validateDept(data){
+    if(data.name == null || data.name === ''){
+        alert('부서명이 올바르지 않습니다.')
+        return false;
     }
-    if(selected != null && data.type === 'dept') {
-        document.querySelectorAll(".dept-btn")
-            .forEach(e=>e.classList.remove("disabled"));
-    } else{
-        document.querySelectorAll(".dept-btn")
-            .forEach(e=>e.classList.add("disabled"));
+
+    if(data.contactNumber == null || data.contactNumber.length !==4){
+        alert('부서 내선번호가 올바르지 않습니다.')
+        return false;
     }
-});
+
+    if(data.upperId == null){
+        alert('상위 부서가 있어야합니다.')
+        return false;
+    }
+
+    return true;
+}
+
+function deptSubmit(type){
+    console.log("submit" , type)
+
+    let data = {
+        name :deptName.value,
+        contactNumber : deptNumber.value,
+        upperId : upperName.value
+    }
+
+    if(!validateDept(data)){
+        return;
+    }
+
+    fetch(`/department/${type}Department`, {
+        method : 'post',
+        headers : {
+            'content-type' : 'application/json;charset=utf-8'
+        },
+        body : JSON.stringify(data)
+    }).then(res => res.json())
+        .then(r => {
+            console.log(r);
+            /* r은 id가 생성된 department 데이터 */
+            data.id = r.id;
+            oc.create(data);
+            bsDeptModal.hide();
+        })
+
+}
+
+deptAddBtn.addEventListener("click",()=>{
+    if(selected == null){
+        return;
+    }
+    fetch('/department/getDepartment?id=' + selected.id)
+        .then(res=>res.json())
+        .then(r=>{
+            console.log(r);
+            setModalAddDept(r);
+            bsDeptModal.show();
+            deptSubmitBtn.addEventListener("click", ()=>deptSubmit('add'));
+        });
+})
+
+createOrgChart();
