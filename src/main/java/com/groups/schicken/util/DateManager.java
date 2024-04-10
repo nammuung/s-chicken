7d54ad4fdd1d60b10dcd913e8ce80d63c8736937
김경모 <gm.kim333@gmail.com>
2024-04-11 05:51:58 +0900

    feat 날짜 매니저 추가

---
+++ src/main/java/com/groups/schicken/util/DateManager.java
@@ -0,0 +1,14 @@
+package com.groups.schicken.util;
+
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class DateManager {
+    public static String getTodayDate() {
+        LocalDate today = LocalDate.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        return today.format(formatter);
+    }
+}
