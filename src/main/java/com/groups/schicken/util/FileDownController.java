ac94989d3079dfe36f0f7c9ba2a46b88f10aca87
beomkim7 <beomkim7@naver.com>
2024-04-15 11:51:16 +0900

    feat:filedown rename

--- src/main/java/com/groups/schicken/util/FileDownController.java
+++ src/main/java/com/groups/schicken/util/FileDownController.java
@@ -5,7 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 @Controller
-public class FileController {
+public class FileDownController {
 
 	@Autowired
 	private FileManager fileManager;
