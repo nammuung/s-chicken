e40c00f81aa9383afbfeb48c98b945a37e53d6a8
nammuung <audrbs2368@naver.com>
2024-04-15 12:06:25 +0900

    feat : fileVO

--- src/main/java/com/groups/schicken/util/FileController.java
+++ src/main/java/com/groups/schicken/util/FileController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.groups.schicken.common.vo.FileVO;
 @Controller
 public class FileController {
 
