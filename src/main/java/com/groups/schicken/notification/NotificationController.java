539f6ce7e3233d79b93cf15f3f4338f406380d61
dongil <az330015@gmail.com>
2024-04-19 11:54:23 +0900

    feat : 알림 만드는 중

--- src/main/java/com/groups/schicken/notification/NotificationController.java
+++ src/main/java/com/groups/schicken/notification/NotificationController.java
@@ -4,12 +4,18 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @RequiredArgsConstructor
 public class NotificationController {
 
     private final SimpMessagingTemplate messagingTemplate;
+
+    @GetMapping("/notification")
+    public String getNotificationPage(){
+        return "notification/notification";
+    }
 
     @MessageMapping("/noti")
     public void sendNotification(NotificationVO notificationVO){
--- src/main/resources/static/js/websocket/notification.js
+++ src/main/resources/static/js/websocket/notification.js
@@ -6,8 +6,62 @@ function mapping(path, callback){
     added[id] = path;
 }
 
-mapping("noti", ({content, time ,occurer}) => {
-    console.log("content : " , content);
-    console.log("time : " , time);
-    console.log("occurer : " , occurer);
-});
+const schickenNotificationList = document.getElementById("schicken-notification-list");
+const notificationBadge = document.getElementById("notification-badge");
+
+
+/* controller */
+mapping("noti", appendNotificationList);
+function appendNotificationList(noti){
+    if(schickenNotificationList.children.length >= 10) {
+        schickenNotificationList.lastElementChild.remove();
+        schickenNotificationList.lastElementChild.remove();
+    }
+    document.querySelectorAll("[data-no-notification]").forEach(e => e.remove());
+
+    notificationBadge.classList.remove("d-none");
+    const listItem = drawNotificationDropdownItem(noti);
+    schickenNotificationList.prepend(listItem);
+}
+
+/* functions */
+function drawNotificationDropdownItem({title, content, time, occurer}){
+    let li = document.createElement("li");
+    li.setAttribute("data-link-occurer", occurer);
+    li.classList.add("notification-item");
+
+    let div = document.createElement("div");
+
+    let h4 = document.createElement("h4");
+    h4.innerText = title;
+
+    let p1 = document.createElement("p");
+    p1.innerText = content;
+
+    let p2 = document.createElement("p");
+    p2.innerText = time;
+
+    div.append(h4, p1, p2);
+    li.append(div);
+
+    let divider = document.createElement("li");
+    let hr = document.createElement("hr");
+    hr.classList.add("dropdown-divider");
+    divider.append(hr);
+
+    let returnDiv = document.createElement("div");
+    returnDiv.append(li, divider);
+
+    return returnDiv;
+}
+
+function onNotificationClick(event){
+    const linkOccurer = event.target.dataset.linkOccurer;
+    if(linkOccurer == null) return;
+
+    console.log(linkOccurer);
+}
+/* 이벤트 리스너등록 */
+schickenNotificationList.addEventListener("click", onNotificationClick);
+
+document.getElementById("notification-icon").addEventListener("click", ()=>notificationBadge.classList.add("d-none"))
