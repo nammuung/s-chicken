2d499eff0e6189cd20fd24c2d7b5770a45fad62a
nammuung <audrbs2368@naver.com>
2024-04-23 15:05:08 +0900

    feat : main 일정관리 작업중

---
+++ src/main/java/com/groups/schicken/CalendarService.java
@@ -0,0 +1,20 @@
+package com.groups.schicken;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureOrder;
+import org.springframework.stereotype.Service;
+
+import com.groups.schicken.annual.AnnualService;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@Slf4j
+public class CalendarService {
+	
+	/*
+	 * @Autowired private CalendarDAO calendarDAO;
+	 */
+	
+	
+}
